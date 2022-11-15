65844800d167eb4c9d7eca66e427021a0421a764
AsadbekAnvarov2305 <asadbekanvarov2305@gmail.com>
2022-11-15 13:59:47 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var elBtn = document.querySelector(".dark-btn")
+var elBody = document.querySelector("body")
+
+elBtn.addEventListener("click", function() {
+    elBody.classList.toggle("active")
+})
+
